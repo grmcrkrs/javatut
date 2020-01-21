abb2499cee7ce15ae2cf3616b74b72bb7079b1c1
grmcrkrs <grmcrkrs@gmail.com>
2020-01-20 21:21:39 -0600

    Change the price to text view to order summary, updated java and xml to more closely reflect the summary changes

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
@@ -85,8 +85,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummary = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummary.setText(message);
     }
 
     /**
