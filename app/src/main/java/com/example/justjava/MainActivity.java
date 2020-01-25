91e3407dc667efb3e3847823c22556bb50d80d25
grmcrkrs <grmcrkrs@gmail.com>
2020-01-24 21:33:21 -0600

    Added checkboxes and comments about initializing and declaring checkbox variables

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -13,6 +13,8 @@ package com.example.justjava;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+//import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -49,20 +51,44 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox checkBox = findViewById(R.id.whippedCreamCheckBox);
+        //initialize the variable for the checkBox that I will be referencing
+        boolean hasWhippedCream = checkBox.isChecked();
+        //initialize the value of the check state of that checkBox
+        // the return type of "isChecked" is type boolean;
+        //the variable name is hasWhippedCream
+        //to check the state of the checkBox, call the method .isChecked on it
+        //checkBox.isChecked() returns true or false, stored in the boolean type variable
+        //hasWhippedCream;
        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+       //in order to use that true or false value stored in boolean hasWhippedCream
+        //in the createOrderSummary method, you must pass that boolean variable
+        //to the createOrderSummary method as an additional argument, see below;
+        //you must also change the createOrderSummary method to expect two arguments
+        //and must pass the right arguments in the right order;
+        CheckBox chocCheckBox = findViewById(R.id.chocolateCheckBox);
+        boolean hasChocolate =chocCheckBox.isChecked();
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
     }
 
     /**
      * this method creates a message that uses price and quantity and returns a string
      * @param price of the order
+     * @param hasWhippedCream boolean checkstate of the whippedCreamCheckBox
      * @return priceMessage
      */
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate) {
+        //this method is expecting the variables /price/ and /hasWhippedCream;
+        //these parameters were passed to the variable in the SubmitOrder method;
+        //this method is being called solely in the SubmitOrder method.
+        //the second variable name does not have to match the variable name
+        //that was passed into arguments previously;
         String priceMessage = "Name: Kaptain Kunal";
         priceMessage = priceMessage + "\nQuantity: " + quantity;
         priceMessage = priceMessage + "\nTotal :$" + price;
+        priceMessage += "\nHas Whipped Cream: " + hasWhippedCream;
+        priceMessage += "\nHas Chocolate Topping: " + hasChocolate;
         priceMessage = priceMessage + "\nThank you!";
         return priceMessage;
     }
@@ -85,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setTextSize(number + 14);
         quantityTextView.setText("" + number);
     }
     /**
