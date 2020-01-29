0749fd6d95c1e834ddd206d96820058903bdec68
grmcrkrs <grmcrkrs@gmail.com>
2020-01-29 13:07:41 -0600

    added some localization strings

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -13,15 +13,11 @@ package com.example.justjava;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -43,13 +39,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = findViewById(R.id.order_summary_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+//    /**
+//     * This method displays the given price on the screen.
+//     */
+//    private void displayPrice(int number) {
+//        TextView priceTextView = findViewById(R.id.order_summary_text_view);
+//        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+//    }
 
     /**
      * This method is called when the order button is clicked.
@@ -64,8 +60,10 @@ public class MainActivity extends AppCompatActivity {
         //on the result. This gives us a String, which we can pass into the createOrderSummary
         //method, which will let us work with it and actually show it on the screen later when the
         //submitOrder method is called.
+
         CheckBox checkBox = findViewById(R.id.whippedCreamCheckBox);
         //initialize the variable for the checkBox that I will be referencing
+
         boolean hasWhippedCream = checkBox.isChecked();
         //initialize the value of the check state of that checkBox
         // the return type of "isChecked" is type boolean;
@@ -80,23 +78,26 @@ public class MainActivity extends AppCompatActivity {
         //and must pass the right arguments in the right order;
         //now that we have added a second checkbox, we had a third input parameter
         //to the createOrderSummary method to pass that variable to that method.
+
         //Then below, in that method we have to change the method signature
         //to look for and accept the third value. But you must tell it what
         //type of value it should accept, telling it in the method signature;
         CheckBox chocCheckBox = findViewById(R.id.chocolateCheckBox);
         boolean hasChocolate = chocCheckBox.isChecked();
         int price = calculatePrice(hasWhippedCream, hasChocolate);
+
         //the next line initializes a variable of type String called priceMessage
         //this variable is set to the return value of createOrderSummary
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, guestName);
+
         //the next line calls the displayMessage method with the input parameter
         //of priceMessage, a string comprised of return value of the createOrderSummary
         //method.
-//        displayMessage(priceMessage);
+//        displayMessage(priceMessage); //no longer displays the message, but sends it to and email
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("*/*");
-        intent.putExtra(Intent.EXTRA_SUBJECT, (("Coffee Order for")+ guestName));
-        intent.putExtra(Intent.EXTRA_TEXT, (priceMessage));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for"+ guestName);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
@@ -117,11 +118,11 @@ public class MainActivity extends AppCompatActivity {
         //the second variable name does not have to match the variable name
         //that was passed into arguments previously;
         String priceMessage = "Name: Crazy " + guestName;
-        priceMessage = priceMessage + "\nQuantity: " + quantity;
-        priceMessage = priceMessage + "\nTotal :$" + price;
-        priceMessage += "\nHas Whipped Cream: " + hasWhippedCream;
-        priceMessage += "\nHas Chocolate Topping: " + hasChocolate;
-        priceMessage = priceMessage + "\nThank you!";
+        priceMessage = priceMessage + "\n" + getString(R.string.quantity_text) + quantity;
+        priceMessage = priceMessage + "\n" + getString(R.string.total) + price;
+        priceMessage += "\n" + getString(R.string.has_whipped_cream) + hasWhippedCream;
+        priceMessage += "\n" + getString(R.string.has_chocolate_topping) + hasChocolate;
+        priceMessage = priceMessage + "\n" + getString(R.string.thank_you);
         return priceMessage;
     }
 
@@ -157,25 +158,25 @@ public class MainActivity extends AppCompatActivity {
 //        quantityTextView.setTextSize(number + 14);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given text on the screen.
-     * @param message expects a String that will be displayed when the method is called
-     */
-    private void displayMessage(String message) {
-        //this method is private, only to be used in this class
-        //void means that the method does not return any value
-        //the name of the method is displayMessage
-        //the input parameter of this method is a string (the variable message
-        //is different than the name of the variable passed to it
-        //but that doesn't matter, only that the String passed to it now has the
-        //variable name /message
-        //the TextView named by findViewById is set to the variable name orderSummary
-        TextView orderSummary = findViewById(R.id.order_summary_text_view);
-        //then the method .setText is called on that TextView /orderSummary
-        //and the input parameter is the String /message that was passed into the
-        //method
-        orderSummary.setText(message);
-    }
+//    /**
+//     * This method displays the given text on the screen.
+//     * @param message expects a String that will be displayed when the method is called
+//     */
+//    private void displayMessage(String message) {
+//        //this method is private, only to be used in this class
+//        //void means that the method does not return any value
+//        //the name of the method is displayMessage
+//        //the input parameter of this method is a string (the variable message
+//        //is different than the name of the variable passed to it
+//        //but that doesn't matter, only that the String passed to it now has the
+//        //variable name /message
+//        //the TextView named by findViewById is set to the variable name orderSummary
+//        TextView orderSummary = findViewById(R.id.order_summary_text_view);
+//        //then the method .setText is called on that TextView /orderSummary
+//        //and the input parameter is the String /message that was passed into the
+//        //method
+//        orderSummary.setText(message);
+//    }
 
     /**
      * this method calculates the price of the order, storing that value in the
